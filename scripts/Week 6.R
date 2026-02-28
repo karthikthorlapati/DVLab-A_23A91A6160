f6a1eca0b02abdc6ca627581125d9be803d23bab
karthikthorlapati <160006709+karthikthorlapati@users.noreply.github.com>
2026-02-28 19:24:44 +0530

    updated week6
    Refactor and enhance visualizations for diamond dataset analysis, including improved scatter plots and hexbin plots with color palettes and faceting.

--- scripts/Week 6.R
+++ scripts/Week 6.R
@@ -1,30 +1,51 @@
-#Load & Understand the Dataset
+#week-
 data(diamonds)
-data(package = .packages(all.available = TRUE))
+data(package =.packages(all.available = TRUE))
 library(ggplot2)
 data(diamonds)
 str(diamonds)
 dim(diamonds)
 ?diamonds
-#Very basic scatter
+
+#very basic scatter
 plot(diamonds$carat, diamonds$price)
-#Improved scatter
-plot( diamonds$carat, diamonds$price, col=rgb(0,0,1,0,1), pch = 16,
-      main = "Scatter Plot: Carat vs Price")
-#Hexbin Using Base R
-install.package("hexbin")
-library(hexbin)
-hb <- hexbin(diamonds$carat, diamonds$price, xbins = 40)
-plot(hb, main = "Hexbin Plot")
-#Basic Hexbin
-ggplot(diamonds, aes(x = carat, y = price))+
-  geom_hex()+
-  labs(title = "Hexagon Binnig: Diamond Structure", x="Carat",y="Price")+
-  theme_minimal()
+
+#improved scatter
+plot(diamonds$carat, diamonds$price,col=rgb(0,0,1,0.1),pch=16,main = "Scatter plot: Carat vs price")
+
+#Hexbin usingbase r
+#install.packages('hexbin')
+#library(hexbin)
+hb<-hexbin(diamonds$carat, diamonds$price,xbins =40)
+plot(hb, main = "hexbin plot")
+
+#basic hexbin
+ggplot(diamonds,aes(x=carat,y=price))+
+  geom_hex()
+
+#add color palette
 ggplot(diamonds,aes(carat,price))+
   geom_hex(bins=40)+
-  scale_fill_gradient(low="lightgreen",high="red")+
-  theme_minimal()
-ggplot(diamonds,aes(carat,price))+geom_hex(bins=35)+scale_fill_viridis_c()+
+  scale_fill_gradient(low="lightgreen",high ="red")+
   theme_minimal()
 
+#professional palette(viridis)
+ggplot(diamonds,aes(carat,price))+
+  geom_hex(bins=35)+
+  scale_fill_viridis_c()+
+  theme_minimal()
+#Add legend title
+ggplot(diamonds, aes(carat, price))+ geom_hex(bins = 20)+
+  scale_fill_viridis_c(name = "Count")+
+  labs( title = "Density Structure of Diamonds", x="Carat", y = "Price") +
+  theme_minimal()
+#Faceted Hexbin
+ggplot(diamonds, aes(carat, price))+ geom_hex() + scale_fill_viridis_c()+
+  facet_wrap(~cut)+
+  theme_minimal()
+#log scale hexbin
+ggplot(diamonds, aes(carat, price))+ geom_hex() + scale_fill_viridis_c()+
+  scale_y_log10()+
+  theme_minimal()
+
+  
