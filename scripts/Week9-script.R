db8049e03156dff0b6183e64ace719c3d5774c49
karthikthorlapati <karthikthorlapati123@gmail.com>
2026-03-12 12:42:21 +0530

    Week 9 script

---
+++ scripts/Week9-script.R
@@ -0,0 +1,66 @@
+#install package
+install.packages("leaflet")
+
+#load the library
+library(leaflet)
+
+#Ver Basic map
+leaflet()%>%addTiles()
+
+#Map Centered on a Location
+leaflet() %>%
+  addTiles() %>%
+  setView(
+    lng = 78.9629,
+    lat = 20.5937,
+    zoom = 5
+  )
+#Add a Marker Example: New Delhi
+leaflet() %>%
+  addTiles() %>%
+  addMarkers(
+    lng = 77.2088,
+    lat = 28.6139
+  )
+
+#marker with popup information
+leaflet() %>%
+  addTiles() %>%
+  addMarkers(
+    lng = 77.2088,
+    lat = 28.6139,
+    popup = "New Delhi"
+  )
+
+#Multiple Markers
+cities<-data.frames(
+  name =c("Delhi","Mumbai","Chennai","Kolkata"),
+  lat = c(28.6139,18.9582,13.0827,22.5744),
+  lng =c(77.2088,72.8321,80.2707,88.3629)
+)
+
+leaflet(cities) %>%
+  addTiles() %>%
+  addMarkers(
+    lng = ~lng,
+    lat = ~lat,
+    popup = ~name
+  )
+
+#Circe Markers (Instead of pins, use circles.)
+leaflet(cities) %>%
+  addTiles() %>%
+  addCircleMarkers(
+    lng = ~lng,
+    lat = ~lat,
+    radius = 6,
+    color = "red",
+    popup = ~name
+  )
+
+#Map with Color Palette
+cities$pop<-c(30,20,11,15)
+pal<-colorNumeric(
+  palette = "red",
+  domain = cities$pop
+)
